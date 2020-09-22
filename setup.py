1bc5959d1b08fa4d40dfb50af1d0de448cdd57dd
anki-code <anki-code@users.noreply.github.com>
2020-09-22 08:59:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setup(
     name='xontrib-argcomplete',
-    version='0.2.9',
+    version='0.2.10',
     license='BSD',
     author='anki-code',
     author_email='xonsh@googlegroups.com',
