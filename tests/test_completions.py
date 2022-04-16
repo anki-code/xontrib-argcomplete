0ca855931b9a83a080390297654fe8274bd74ec1
Noortheen Raja <jnoortheen@gmail.com>
2022-04-16 15:12:28 +0530

    feat: complete `python script.py` patterns

--- tests/test_completions.py
+++ tests/test_completions.py
@@ -1,38 +1,72 @@
-from pytest import fixture
+import pytest
 
 
-@fixture(autouse=True)
-def argcomplete(load_xontrib, xession):
+@pytest.fixture(autouse=True)
+def argcomplete(load_xontrib, xession, os_env):
+    xession.env["PATH"] = os_env["PATH"]
     return load_xontrib("argcomplete")
 
 
-def test_cmd_pytest(check_completer, os_env, xession):
-    xession.env["PATH"] = os_env["PATH"]
+def test_cmd_pytest(check_completer):
     out = check_completer("pytest", prefix="-")
-    assert out.issuperset({
-        '--log-format',
-        '--log-level',
-        '--markers',
-        '--maxfail',
-        '--new-first',
-        '--nf',
-        '--no-header',
-        '--no-summary',
-        '--noconftest',
-        '--override-ini',
-        '--pastebin',
-        '--pdb',
-        '--pdbcls',
-        '--pyargs',
-        '--pythonwarnings',
-        '--quiet',
-        '--rootdir',
-        '--runxfail',
-        '--setup-only',
-        '--setup-plan',
-        '--setup-show',
-        '--setuponly',
-        '--setupplan',
-        '--setupshow',
-        '--show-capture',
-    })
+    assert out.issuperset(
+        {
+            "--log-format",
+            "--log-level",
+            "--markers",
+            "--maxfail",
+            "--new-first",
+            "--nf",
+            "--no-header",
+            "--no-summary",
+            "--noconftest",
+            "--override-ini",
+            "--pastebin",
+            "--pdb",
+            "--pdbcls",
+            "--pyargs",
+            "--pythonwarnings",
+            "--quiet",
+            "--rootdir",
+            "--runxfail",
+            "--setup-only",
+            "--setup-plan",
+            "--setup-show",
+            "--setuponly",
+            "--setupplan",
+            "--setupshow",
+            "--show-capture",
+        }
+    )
+
+
+class TestScripts:
+    """test executing a script"""
+
+    @pytest.mark.parametrize(
+        "cmd, file",
+        [
+            ("python", "proto.py"),
+            pytest.param(
+                "xonsh",
+                "proto.xsh",
+                marks=pytest.mark.xfail(
+                    reason="the xompletions/xonsh is interfering and will fail"
+                ),
+            ),
+        ],
+    )
+    def test_executing_scripts(self, check_completer, cmd, file):
+        out = check_completer(f"{cmd} {file}", prefix="-")
+        assert out.issuperset({"--proto"})
+
+    @pytest.mark.parametrize(
+        "cmd",
+        [
+            "./proto.py",
+            "./proto.xsh",
+        ],
+    )
+    def test_shebang_scripts(self, check_completer, cmd, file):
+        out = check_completer(f"{cmd} {file}", prefix="-")
+        assert out.issuperset({"--proto"})
--- xontrib/argcomplete.py
+++ xontrib/argcomplete.py
@@ -9,15 +9,16 @@ from xonsh.built_ins import XonshSession, XSH
 from xonsh.completers import completer
 from xonsh.completers.tools import (
     contextual_command_completer,
-    RichCompletion,
+    completion_from_cmd_output,
 )
 from xonsh.parsers.completion_context import CommandContext
 
 # without this, all names will be imported in the context
 __all__ = ()
 
+
 def _get_executor(arg):
-    m = re.match("^(python[0-9.]*|xonsh)$", arg)
+    m = re.match("^(python[0-9.]*|xonsh|pypy[0-9.]*)$", arg)
     return m.group(1) if m else None
 
 
@@ -25,88 +26,18 @@ def _get_filepath(arg):
     return arg.strip("'").rstrip("\n")
 
 
-# Run something, muting output or redirecting it to the debug stream
-# depending on the value of _ARC_DEBUG.
-def _python_argcomplete_run():
-    # if [[ -z "${ARGCOMPLETE_USE_TEMPFILES-}" ]]; then
-    #     __python_argcomplete_run_inner "$@"
-    #     return
-    # fi
-    # local tmpfile="$(mktemp)"
-    # _ARGCOMPLETE_STDOUT_FILENAME="$tmpfile" __python_argcomplete_run_inner "$@"
-    # local code=$?
-    # cat "$tmpfile"
-    # rm "$tmpfile"
-    # return $code
-    sp.check_output(args=[])
-
-
 # Scan the beginning of an executable file ($1) for a regexp ($2). By default,
 # scan for the magic string indicating that the executable supports the
 # argcomplete completion protocol. By default, scan the first kilobyte;
 # if $3 is set to -n, scan until the first line break up to a kilobyte.
-def _python_argcomplete_scan_head(file: Path) -> bool:
-    with file.open("rb") as reader:
+def _python_argcomplete_scan_head(file: str) -> bool:
+    with open(file, "rb") as reader:
         txt = reader.read(1024)
         # todo: implement reading until line-breakup
     return b"PYTHON_ARGCOMPLETE_OK" in txt
 
 
-def _complete_script_file():
-    """"""
-
-    # if [[ "$executable" == python* ]] || [[ "$executable" == pypy* ]]; then
-    #     if [[ "${COMP_WORDS[1]}" == -m ]]; then
-    #         if __python_argcomplete_run "$executable" -m argcomplete._check_module "${COMP_WORDS[2]}"; then
-    #             ARGCOMPLETE=3
-    #         else
-    #             return
-    #         fi
-    #     elif [[ -f "${COMP_WORDS[1]}" ]] && __python_argcomplete_scan_head_noerr "${COMP_WORDS[1]}"; then
-    #         local ARGCOMPLETE=2
-
-
-def _complete_executable():
-    """
-    Handle two cases, where
-        1. the executable file is actually a Python script (``$ pytest`` ...)
-        2. the script file with a shebang (``$ ./script.py`` ...)
-    """
-
-    # elif type -P "$executable" >/dev/null 2>&1; then
-    #     local SCRIPT_NAME=$(type -P "$executable")
-    #     if (type -t pyenv && [[ "$SCRIPT_NAME" = $(pyenv root)/shims/* ]]) >/dev/null 2>&1; then
-    #         local SCRIPT_NAME=$(pyenv which "$executable")
-    #     fi
-    #     if __python_argcomplete_scan_head_noerr "$SCRIPT_NAME"; then
-    #         local ARGCOMPLETE=1
-    #     elif __python_argcomplete_scan_head_noerr "$SCRIPT_NAME" '^#!(.*)$' -n && [[ "${BASH_REMATCH[1]}" =~ ^.*(python|pypy)[0-9\.]*$ ]]; then
-    #         local interpreter="$BASH_REMATCH"
-    #         if (__python_argcomplete_scan_head_noerr "$SCRIPT_NAME" "(PBR Generated)|(EASY-INSTALL-(SCRIPT|ENTRY-SCRIPT|DEV-SCRIPT))" \
-    #             && "$interpreter" "$(type -P python-argcomplete-check-easy-install-script)" "$SCRIPT_NAME") >/dev/null 2>&1; then
-    #             local ARGCOMPLETE=1
-    #         elif __python_argcomplete_run "$interpreter" -m argcomplete._check_console_script "$SCRIPT_NAME"; then
-    #             local ARGCOMPLETE=1
-    #         fi
-    #     fi
-    # fi
-
-
-def create_rich_completion(line: str):
-    line = line.strip()
-    if "\t" in line:
-        cmd, desc = map(str.strip, line.split("\t", maxsplit=1))
-    else:
-        cmd, desc = line, ""
-    return RichCompletion(
-        str(cmd),
-        description=str(desc),
-        append_space=True,
-    )
-
-
-def _run_binary(exe: str, line: str, begidx: "int|None" = None) -> "None|str":
-    args = [exe]
+def arg_complete_proc(line: str, *args: str, begidx: "int|None" = None) -> "None|str":
     comp_point = str(len(line)) if begidx is None else str(begidx)
 
     outfile = tempfile.NamedTemporaryFile(delete=False)
@@ -127,7 +58,7 @@ def _run_binary(exe: str, line: str, begidx: "int|None" = None) -> "None|str":
         # pytest 8>&1 9>&2 1>/dev/null 2>&1
         # fd 9 is used for debug output
         sp.run(
-            [args[0]],
+            args,
             stderr=sp.DEVNULL,
             stdout=sp.DEVNULL,
             env=env,
@@ -139,11 +70,11 @@ def _run_binary(exe: str, line: str, begidx: "int|None" = None) -> "None|str":
     return out
 
 
-def _get_completions(exe: str, line: str, begidx: "int|None" = None):
-    output = _run_binary(exe, line, begidx)
+def _get_completions(line: str, *args: str, begidx: "int|None" = None):
+    output = arg_complete_proc(line, *args, begidx=begidx)
     if output:
         for line in output.strip().splitlines(keepends=False):
-            yield create_rich_completion(line)
+            yield completion_from_cmd_output(line, append_space=True)
 
 
 @contextual_command_completer
@@ -152,12 +83,25 @@ def python_argcomplete(ctx: CommandContext):
     if not ctx.args:
         return
 
+    cmd = ctx.args[0].value
     line = ctx.text_before_cursor
-    executable = ctx.args[0].value
+
+    args = None
+    if ctx.arg_index > 1 and _get_executor(cmd):
+        # Handle the case where the script is executed with Python/xonsh binary
+        file_path = ctx.args[1].value
+        if _python_argcomplete_scan_head(file_path):
+            args = [cmd, file_path]
+
     defined_exes = XSH.env.get("XONSH_ARGCOMPLETE_COMMANDS") or set()
-    if executable not in defined_exes:
-        return
-    return _get_completions(executable, line, ctx.begidx), False
+    if cmd in defined_exes:
+        # Handle the case where the first argument is an executable
+        args = [cmd]
+
+    if args:
+        # todo: return dict once supported
+        result = _get_completions(line, *args, begidx=ctx.begidx)
+        return result, False  # not filtered
 
 
 def xonsh_entrypoint(xsh: XonshSession, **_):
@@ -178,5 +122,9 @@ xonsh_entrypoint(XSH)
 
 
 # todo:
-#  1. global completion ( disabled by default. can be controlled by env variable)
+#  1. global completion ( disabled by default.
+#       can be controlled by env variable as it will make other completions slow as well)
+#       - https://kislyuk.github.io/argcomplete/#global-completion
 #  2. ./script.py or python script.py completion. it should be called with subprocess with tmp-file path completion
+#       ii. the script file with a shebang (``$ ./script.py`` ...)
+#
