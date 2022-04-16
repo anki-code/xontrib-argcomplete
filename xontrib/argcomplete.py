dcdadefc70ab48fb82c886990a60f3b9f541804e
Noortheen Raja <jnoortheen@gmail.com>
2022-04-16 12:53:57 +0530

    feat: implement commands completions

--- xontrib/argcomplete.py
+++ xontrib/argcomplete.py
@@ -1,107 +1,181 @@
 #!/usr/bin/env xonsh
-
+import os
 import re
+import subprocess as sp
+import tempfile
 from pathlib import Path
-from shutil import which
 
-def _get_subproc_output(cmds, debug=False):
-    if not debug:
-        cmds += ['2>', '/dev/null']    
-    result = __xonsh__.subproc_captured_object(cmds)
-    result.rtn  # workaround https://github.com/xonsh/xonsh/issues/3394
-    return result.output
+from xonsh.built_ins import XonshSession
+from xonsh.completers import completer
+from xonsh.completers.tools import (
+    contextual_command_completer,
+    RichCompletion,
+)
+from xonsh.parsers.completion_context import CommandContext
+
 
 def _get_executor(arg):
-    m = re.match('^(python[0-9.]*|xonsh)$', arg)
+    m = re.match("^(python[0-9.]*|xonsh)$", arg)
     return m.group(1) if m else None
 
+
 def _get_filepath(arg):
-    return arg.strip("'").rstrip('\n')
+    return arg.strip("'").rstrip("\n")
 
-def _xontrib_argcomplete_completer(prefix, line, begidx, endidx, ctx):
+
+# Run something, muting output or redirecting it to the debug stream
+# depending on the value of _ARC_DEBUG.
+def _python_argcomplete_run():
+    # if [[ -z "${ARGCOMPLETE_USE_TEMPFILES-}" ]]; then
+    #     __python_argcomplete_run_inner "$@"
+    #     return
+    # fi
+    # local tmpfile="$(mktemp)"
+    # _ARGCOMPLETE_STDOUT_FILENAME="$tmpfile" __python_argcomplete_run_inner "$@"
+    # local code=$?
+    # cat "$tmpfile"
+    # rm "$tmpfile"
+    # return $code
+    sp.check_output(args=[])
+
+
+# Scan the beginning of an executable file ($1) for a regexp ($2). By default,
+# scan for the magic string indicating that the executable supports the
+# argcomplete completion protocol. By default, scan the first kilobyte;
+# if $3 is set to -n, scan until the first line break up to a kilobyte.
+def _python_argcomplete_scan_head(file: Path) -> bool:
+    with file.open("rb") as reader:
+        txt = reader.read(1024)
+        # todo: implement reading until line-breakup
+    return b"PYTHON_ARGCOMPLETE_OK" in txt
+
+
+def _complete_script_file():
+    """"""
+
+    # if [[ "$executable" == python* ]] || [[ "$executable" == pypy* ]]; then
+    #     if [[ "${COMP_WORDS[1]}" == -m ]]; then
+    #         if __python_argcomplete_run "$executable" -m argcomplete._check_module "${COMP_WORDS[2]}"; then
+    #             ARGCOMPLETE=3
+    #         else
+    #             return
+    #         fi
+    #     elif [[ -f "${COMP_WORDS[1]}" ]] && __python_argcomplete_scan_head_noerr "${COMP_WORDS[1]}"; then
+    #         local ARGCOMPLETE=2
+
+
+def _complete_executable():
     """
-    Argcomplete support to tab completion of python and xonsh scripts in xonsh shell.
+    Handle two cases, where
+        1. the executable file is actually a Python script (``$ pytest`` ...)
+        2. the script file with a shebang (``$ ./script.py`` ...)
     """
-    debug = __xonsh__.env.get('XONTRIB_ARGCOMPLETE_DEBUG', False)
-    py = None
-    file = None
 
-    args = __xonsh__.execer.parser.lexer.split(line)
+    # elif type -P "$executable" >/dev/null 2>&1; then
+    #     local SCRIPT_NAME=$(type -P "$executable")
+    #     if (type -t pyenv && [[ "$SCRIPT_NAME" = $(pyenv root)/shims/* ]]) >/dev/null 2>&1; then
+    #         local SCRIPT_NAME=$(pyenv which "$executable")
+    #     fi
+    #     if __python_argcomplete_scan_head_noerr "$SCRIPT_NAME"; then
+    #         local ARGCOMPLETE=1
+    #     elif __python_argcomplete_scan_head_noerr "$SCRIPT_NAME" '^#!(.*)$' -n && [[ "${BASH_REMATCH[1]}" =~ ^.*(python|pypy)[0-9\.]*$ ]]; then
+    #         local interpreter="$BASH_REMATCH"
+    #         if (__python_argcomplete_scan_head_noerr "$SCRIPT_NAME" "(PBR Generated)|(EASY-INSTALL-(SCRIPT|ENTRY-SCRIPT|DEV-SCRIPT))" \
+    #             && "$interpreter" "$(type -P python-argcomplete-check-easy-install-script)" "$SCRIPT_NAME") >/dev/null 2>&1; then
+    #             local ARGCOMPLETE=1
+    #         elif __python_argcomplete_run "$interpreter" -m argcomplete._check_console_script "$SCRIPT_NAME"; then
+    #             local ARGCOMPLETE=1
+    #         fi
+    #     fi
+    # fi
 
-    if len(args) == 0:
-        return None
 
-    py = _get_executor(args[0])
-    if py:
-        if len(args) > 1:
-            file = _get_filepath(args[1])
-        else:
-            return None if not debug else ((prefix, 'xontrib-argcomplete DEBUG: file not found'), len(prefix))
+def create_rich_completion(line: str):
+    line = line.strip()
+    if "\t" in line:
+        cmd, desc = map(str.strip, line.split("\t", maxsplit=1))
     else:
-        file = _get_filepath(args[0])
+        cmd, desc = line, ""
+    return RichCompletion(
+        str(cmd),
+        description=str(desc),
+        append_space=True,
+    )
 
-        if file.endswith('.xsh'):
-            py = 'xonsh'
-        elif file.endswith('.py'):
-            py = 'python'
 
-    if not Path(file).exists():
-        which_maybe_file = which(file)
-        if which_maybe_file and Path(which_maybe_file).exists():
-            file = str(which_maybe_file)
-        else:
-            try:
-                which_maybe_file = _get_subproc_output(['which', file], debug).strip()
-            except:
-                return None
-            if which_maybe_file and Path(which_maybe_file).exists():
-                file = which_maybe_file
+def _run_binary(exe: str, line: str, begidx: "int|None" = None) -> "None|str":
+    args = [exe]
+    comp_point = str(len(line)) if begidx is None else str(begidx)
 
-    if not file:
-        return None if not debug else ((prefix, 'xontrib-argcomplete DEBUG: path to file not found'), len(prefix))
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    outfile.close()
+    env = dict(
+        _ARGCOMPLETE="1",
+        _ARGCOMPLETE_IFS="\n",
+        _ARGCOMPLETE_DFS="\t",
+        _ARGCOMPLETE_SUPPRESS_SPACE="1",
+        _ARGCOMPLETE_SHELL="fish",
+        _ARGCOMPLETE_STDOUT_FILENAME=outfile.name,  # or fd=8 is used
+        COMP_LINE=line,
+        COMP_POINT=comp_point,
+    )
+    env.update(XSH.env.detype())
 
-    if not Path(file).exists():
-        return None if not debug else ((prefix, 'xontrib-argcomplete DEBUG: file does not exists'), len(prefix))
+    out = None
+    try:
+        # pytest 8>&1 9>&2 1>/dev/null 2>&1
+        # fd 9 is used for debug output
+        sp.run(
+            [args[0]],
+            stderr=sp.DEVNULL,
+            stdout=sp.DEVNULL,
+            env=env,
+        )
+        out = Path(outfile.name).read_bytes().decode()
+    finally:
+        os.unlink(outfile.name)
 
-    file_type = _get_subproc_output(['file', '--mime-type', '--brief', file], debug).strip()
-    if not file_type.startswith('text'):
-        return None if not debug else ((prefix, f'xontrib-argcomplete DEBUG: file type is not text: {file_type}'), len(prefix))
+    return out
 
-    found_argcomplete = False
-    with open(file) as f:
-        for x in range(10):
-            try:
-                fline = next(f)
-            except:
-                break
-            if x == 0 and not py:
-                m = re.match('.*env (python[0-9.]*|xonsh).*', fline)
-                if m:
-                    py = m.group(1)
-                else:
-                    m = re.match('#!(/.*/(python[0-9.]*|xonsh))', fline)
-                    if m:
-                        py = m.group(1)
-                        if not Path(py).exists():
-                            if debug:
-                                return ((prefix, 'xontrib-argcomplete DEBUG: the python/xonsh path in the script`s shebang does not exists'), len(prefix))
-                            py = None
 
-            if 'PYTHON_ARGCOMPLETE_OK' in fline:
-                found_argcomplete = True
-                break
+def _get_completions(exe: str, line: str, begidx: "int|None" = None):
+    output = _run_binary(exe, line, begidx)
+    if output:
+        for line in output.strip().splitlines(keepends=False):
+            yield create_rich_completion(line)
 
-    if found_argcomplete and py:
-        with __xonsh__.env.swap(_ARGCOMPLETE=str(1), _ARGCOMPLETE_IFS='\n', COMP_LINE=str(line), COMP_POINT=str(begidx)):
-            output = _get_subproc_output(['bash', '-c', f"{py} '{file}' 8>&1"])
-        tokens = set([t.replace(r'\ ', ' ') for t in output.split('\n') if prefix in t])
 
-        if len(tokens) == 0:
-            return None if not debug else ((prefix, 'xontrib-argcomplete DEBUG: completions not found'), len(prefix))
+@contextual_command_completer
+def python_argcomplete(ctx: CommandContext):
+    """Argcomplete-https://kislyuk.github.io/argcomplete/ support"""
+    if not ctx.args:
+        return
 
-        return (tokens, len(prefix))
+    line = ctx.get_words()
+    if ctx.prefix:
+        line += " " + ctx.prefix
 
-    return None
+    executable = ctx.args[0].raw_value
+    return _get_completions(executable, line, ctx.begidx), False
 
-__xonsh__.completers['xontrib_argcomplete'] = _xontrib_argcomplete_completer
-__xonsh__.completers.move_to_end('xontrib_argcomplete', last=False)
+
+def xonsh_entrypoint(xsh: XonshSession, **_):
+    # todo: xsh.completers.add_one_completer
+    completer.add_one_completer("argcomplete", python_argcomplete, "<import")
+
+
+if __name__ == "__main__":
+    from xonsh.built_ins import XSH
+
+    XSH.load()
+    xonsh_entrypoint(XSH)
+    out = _get_completions("pytest", "pytest -")
+    from pprint import pprint
+
+    pprint(list(out))
+
+
+# todo:
+#  1. global completion ( disabled by default. can be controlled by env variable)
+#  2. ./script.py or python script.py completion. it should be called with subprocess with tmp-file path completion
+#  3. integrate this to xontrib-argcomplete
